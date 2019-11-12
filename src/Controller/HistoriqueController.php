9195b89eac5e6da15a1c488a2db04663aeeddec6
kaira <husaynmozart@gmail.com>
2019-11-12 14:01:48 +0300

    avancement

--- src/Controller/HistoriqueController.php
+++ src/Controller/HistoriqueController.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Knp\Component\Pager\PaginatorInterface;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 /**
  * @Route("/historique")
