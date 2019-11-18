5348b8a657b138420a8f86c1233e6eb8f93fb267
kaira <husaynmozart@gmail.com>
2019-11-18 07:26:48 +0300

    reception dgsr

--- src/Migrations/Version20191118042557.php
+++ src/Migrations/Version20191118042557.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191117060328 extends AbstractMigration
+final class Version20191118042557 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -23,6 +23,9 @@ final class Version20191117060328 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, role VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE utilisateur ADD role_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE utilisateur ADD CONSTRAINT FK_1D1C63B3D60322AC FOREIGN KEY (role_id) REFERENCES role (id)');
+        $this->addSql('CREATE INDEX IDX_1D1C63B3D60322AC ON utilisateur (role_id)');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +33,9 @@ final class Version20191117060328 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE utilisateur DROP FOREIGN KEY FK_1D1C63B3D60322AC');
         $this->addSql('DROP TABLE role');
+        $this->addSql('DROP INDEX IDX_1D1C63B3D60322AC ON utilisateur');
+        $this->addSql('ALTER TABLE utilisateur DROP role_id');
     }
 }
